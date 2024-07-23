d3c0e33505c0a2e7f45377ac0b66f9cee054fdaa
Ryan <tjddnfkdls0@naver.com>
2024-07-23 15:03:38 +0900

    ProfileCard

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import axios from 'axios'
-import Main from "./components/atomic/pages/Main";
+import ProfileCard_Test from "./components/atomic/pages/ProfileCard_Test";
 // export let mainDomain = 'http://localhost:8000'
 
 export let mainDomain = ''
@@ -13,7 +13,7 @@ export default () => {
     <BrowserRouter>
       <Routes>
         <Route>
-          <Route path="/" element={<Main />} />
+          <Route path="/" element={<ProfileCard_Test />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/components/atomic/molecules/ProfileItem.js
+++ src/components/atomic/molecules/ProfileItem.js
@@ -3,14 +3,49 @@ import styled from 'styled-components'
 import BaseImage, { Tag } from '../atoms/BaseImage'
 import { Paragraph, Title } from '../atoms/Typograpy'
 
-// 문제
-/**
- * 
- */
+const Container = styled.div`
+  display : flex;
+  border : 1px solid #ccc;
+  padding-top : 12px;
+  padding-bottom : 12px;
+`
+const ProfileImage = styled(BaseImage)`
+  width : 100px;
+  height : 100px;
+  margin-left : 20px;
+`
+const Wrap = styled.div`
+  margin-top : auto;
+  margin-bottom: auto;
+  margin-left : 40px;
+  line-height : 1.5;
+  
+`
+const UserName = styled(Title)`
+  font-weight : bold;
+  font-size : 24px;
+`
+const StyledTag = styled(Tag)`
+  font-size : 12px;
+  margin-right : 6px;
+  padding : 2px 4px 2px 4px;
+`
 
 
 export default props => {
   return (
-    <div>dkdkdk</div>
+    <Container>
+      <ProfileImage src={props.src}/>
+        <Wrap>
+          <UserName>{props.title}</UserName>
+          <div style={{display : 'flex'}}>
+            {props.tags.map(tag =>   // tags라는 데이터
+                <StyledTag>{tag}</StyledTag>
+            )}
+          </div>
+          <Paragraph>{props.content}</Paragraph>
+          {props.children} 
+        </Wrap>
+    </Container>
   )
 }
