aa6a1b4052c737cdcf0e3608da1804547d641968
Ryan <tjddnfkdls0@naver.com>
2024-07-23 19:03:35 +0900

    Header bg fix

--- src/components/atomic/atoms/TopBar.js
+++ src/components/atomic/atoms/TopBar.js
@@ -5,7 +5,6 @@ import styled from 'styled-components'
 const Container = styled.div`
     width : 100%;
     height : 60px;
-    background-color : #eee;
     display : flex;
     justify-content: space-between;
     align-items: center;
