5d2aadf31f2345a159eafce5b7a3f752acb6c8f8
modaing <hongi_@naver.com>
2024-07-24 02:30:57 +0900

    LiveChat 페이지 구현 + ai 프롬프트 메세지 추가

--- src/components/atomic/atoms/ChatMessage.js
+++ src/components/atomic/atoms/ChatMessage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ORIGINAL_MESSAGE, ORIGINAL_PARTNER_MESSAGE, TRANSLATED_MESSAGE, TRANSLATED_PARTNER_MESSAGE } from "./Color";
+import { AI_MESSAGE, ORIGINAL_MESSAGE, ORIGINAL_PARTNER_MESSAGE, TRANSLATED_MESSAGE, TRANSLATED_PARTNER_MESSAGE } from "./Color";
 import styled from "styled-components";
 
 const CHAT_MESSAGE_STYLE = {
@@ -19,24 +19,36 @@ const CHAT_MESSAGE_STYLE = {
     'partner-translated':`
         background-color: ${TRANSLATED_PARTNER_MESSAGE};
         color: white;
+    `,
+    'ai':`
+        background-color: ${AI_MESSAGE};
+        color: black;
+        padding-bottom: 20px;
     `
 }
 export const ChatMessage = styled.div`
     display: block;
     padding: 15px 12px;
     border-radius: 8px;
+    line-height: 1.5;
     ${props => {
         if (props.type === 'me' && props.isOriginal) {
             return CHAT_MESSAGE_STYLE['me-original']
 
-        } else if (props.type === 'me') {
+        } else if (props.type === 'me' && props.isOriginal == false) {
             return CHAT_MESSAGE_STYLE['me-translated']
 
         } else if (props.type === 'partner' && props.isOriginal) {
             return CHAT_MESSAGE_STYLE['partner-original']
 
-        } else {
+        } else if (props.type === 'partner' && props.isOriginal == false) {
             return CHAT_MESSAGE_STYLE['partner-translated']
+
+        } else if (props.type === 'ai') {
+            return CHAT_MESSAGE_STYLE['ai']
+        
+        } else {
+            return null
         }
     }}
     `;
--- src/components/atomic/molecules/ChatForm.js
+++ src/components/atomic/molecules/ChatForm.js
@@ -17,7 +17,37 @@ function ChatForm() {
             messageSenderId: 950326,
             originalMessage: 'I had a pretty normal day. Just went to work and came back home.',
             traslatedMessage: '오늘 하루는 그냥 그랬어. 일하고 다시 퇴근했어.'
+        },
+        {
+            type: 'ai',
+            aiMessageType: 'topic',
+            aiMessageContents: '오늘 어떤 하루를 보냈는지 물어보세요!'
+        },
+        {
+            type: 'ai',
+            aiMessageType: 'expression',
+            aiMessageContents: '답변으로 추천하는 표현입니다.\
+                                1. 일상적인 하루\
+                                "I had a pretty normal day today."\
+                                "Today was just another typical day."\
+                                바쁜 하루\
+                                "I had a really busy day today."\
+                                "My day was jam-packed with activities."\
+                                스트레스 받은 하루\
+                                "Today was quite stressful."\
+                                "I had a tough day today."\
+                                즐거운 하루\
+                                "I had a wonderful day."\
+                                "Today was really enjoyable."\
+                                생산적인 하루\
+                                "I had a very productive day."\
+                                "Today was very fruitful."\
+                                느긋한 하루\
+                                "I had a relaxing day."\
+                                "Today was very laid-back."\
+                                '
         }
+
     ];
 
     const ChatCard = styled.div`
@@ -28,35 +58,65 @@ function ChatForm() {
         overflow-y: auto;
         padding: 10px;
         border-radius: 8px;
+        ::-webkit-scrollbar {
+            width: 0px;
+            background: transparent; /* make scrollbar transparent */
+        }
+        -ms-overflow-style: none;  /* IE and Edge */
+        scrollbar-width: none;  /* Firefox */
     `;
 
     const ChatMessageWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: ${props => props.type === 'me' ? 'flex-end' : 'flex-start'};
-    margin-bottom: ${props => (props.isDifferentType ? 'none' : '30px')};
+        display: flex;
+        flex-direction: column;
+        align-items: ${props => props.type === 'me' ? 'flex-end' : 'flex-start'};
+        margin-bottom: ${props => (props.isDifferentType ? 'none' : '30px')};
+    `;
+    const AiMessageWrapper = styled.div`
+        flex-direction: column;
+        margin-bottom: 30px;
+        align-items: flex-start;
+        display: flex;
     `;
 
     return (
         <ChatCard>
             {data && data?.map((message, index) => {
                 const isDifferentType = index > 0 && data[index - 1].type !== message.type;
-                
+
                 return (
-                    <ChatMessageWrapper key={index} type={message.type} isDifferentType={isDifferentType}>
-                        <ChatMessage
-                            type={message?.type}
-                            isOriginal={true}
-                        >
-                            {message?.originalMessage}
-                        </ChatMessage>
-                        <ChatMessage
-                            type={message?.type}
-                            isOriginal={false}
-                        >
-                            {message?.traslatedMessage}
-                        </ChatMessage>
-                    </ChatMessageWrapper>
+                    <React.Fragment key={index}>
+                        {(message.type === 'partner' || message.type === 'me') && (
+                            <ChatMessageWrapper key={index} type={message.type} isDifferentType={isDifferentType}>
+                                <ChatMessage
+                                    type={message?.type}
+                                    isOriginal={true}
+                                >
+                                    {message?.originalMessage}
+                                </ChatMessage>
+                                <ChatMessage
+                                    type={message?.type}
+                                    isOriginal={false}
+                                >
+                                    {message?.traslatedMessage.split('.').map((sentence, i) => (
+                                        <span key={i}>{sentence.trim()}<br /></span>
+                                    ))}
+                                </ChatMessage>
+                            </ChatMessageWrapper>
+                        )}
+                        {message.type === 'ai' && (
+                            <AiMessageWrapper>
+                                <ChatMessage
+                                    type={message?.type}
+                                    aiMessageType={message?.aiMessageType}
+                                >
+                                    {message?.aiMessageContents.split('.').map((sentence, i) => (
+                                        <span key={i}>{sentence.trim()}<br /></span>
+                                    ))}
+                                </ChatMessage>
+                            </AiMessageWrapper>
+                        )}
+                    </React.Fragment>
                 )
             })}
         </ChatCard>
