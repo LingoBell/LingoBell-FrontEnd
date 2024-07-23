cfebb578ea5b984ba798c4d066e08bdd32cc0376
Ryan <tjddnfkdls0@naver.com>
2024-07-23 13:05:02 +0900

    add

---
+++ src/components/atomic/atoms/ButtonCheckBox.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const ButtonCheckBox = styled.div`
+  background-color: #000;
+  color: white;
+  
+
+`
+
+export default ButtonCheckBox
