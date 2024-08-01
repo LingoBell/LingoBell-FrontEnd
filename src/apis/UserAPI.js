35ede60ef7535a4588ce6128a1b601f073fb38a4
Ryan <tjddnfkdls0@naver.com>
2024-08-01 15:38:43 +0900

    user auth api modified

--- src/apis/UserAPI.js
+++ src/apis/UserAPI.js
@@ -25,3 +25,20 @@ export const GetRequestUserList = async () => {
         throw error;
     }
 };
+
+
+export const AddUserProfile = async (formData) => {
+    try {
+        const response = await axios.post('users',
+            formData, {
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        }
+        );
+        console.log('Success:', response.data)
+    } catch (error) {
+        console.log('Error:', error)
+    }
+}
+
