e15556a7191bc96aa15b5cf2653ea5fd99c16fff
modaing <hongi_@naver.com>
2024-08-07 10:39:57 +0900

    video에 facelandmark 추가

--- src/components/atomic/pages/Video.js
+++ src/components/atomic/pages/Video.js
@@ -2,16 +2,31 @@ import React, { forwardRef, useEffect, useImperativeHandle, useRef, useState } f
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components'
 import io from "socket.io-client";
+import { FaceLandmarker, FilesetResolver, DrawingUtils } from '@mediapipe/tasks-vision';
 
 const socket = io('')
 let pc1 = new RTCPeerConnection()
 let pc = null
 let isCaller = false
+
+const VideoContainer = styled.div`
+  position: relative;
+  display: inline-block;
+`;
+
+const Canvas = styled.canvas`
+  position: absolute;
+  top: 0;
+  left: 0;
+  pointer-events: none;
+  z-index: 10;
+`;
+
 const Video = forwardRef((props, ref) => {
     const params = useParams()
     const {
         notifyDiscon,
-        onAudioStatusChange, 
+        onAudioStatusChange,
         onVideoStatusChange
     } = props
     const {
@@ -24,9 +39,29 @@ const Video = forwardRef((props, ref) => {
     const peerConnection = useRef(null)
     const [isAudioEnabled, setIsAudioEnabled] = useState(true);
     const [isVideoEnabled, setIsVideoEnabled] = useState(true);
-    
+    const [faceLandmarker, setFaceLandmarker] = useState(null);
+    // const [webcamRunning, setWebcamRunning] = useState(false);
+    const canvasRef = useRef(null);
+
+    const initFaceLandmarker = async () => {
+        const filesetResolver = await FilesetResolver.forVisionTasks(
+            'https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3/wasm'
+        );
+        const landmarker = await FaceLandmarker.createFromOptions(filesetResolver, {
+            baseOptions: {
+                modelAssetPath: `https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task`,
+                delegate: "CPU"
+            },
+            outputFaceBlendshapes: true,
+            runningMode: "VIDEO",
+            numFaces: 1
+        });
+        setFaceLandmarker(landmarker);
+    }
+
     const init = async () => {
         console.log('init start')
+        await initFaceLandmarker();
         const stream = await navigator.mediaDevices.getUserMedia({
             video: true,
             // audio: true,
@@ -74,7 +109,7 @@ const Video = forwardRef((props, ref) => {
         socket.on('ANSWER_RECEIVED', async (answer) => {
             console.log('ANSWER_RECEIVED')
             await pc.setRemoteDescription(new RTCSessionDescription(answer))
-            
+
         })
         socket.on('OFFER_RECEIVED', async (offer) => {
             console.log('OFFER_RECEIVED')
@@ -94,7 +129,7 @@ const Video = forwardRef((props, ref) => {
             console.log('OPP_DISCON')
             isCaller = true
             if (pc) {
-                
+
                 // pc.setRemoteDescription(null)
                 pc.close()
                 pc = null
@@ -106,6 +141,7 @@ const Video = forwardRef((props, ref) => {
 
         socket.emit('CREATE_OR_JOIN', roomName)
     }
+
     useEffect(() => {
         init()
 
@@ -115,14 +151,14 @@ const Video = forwardRef((props, ref) => {
             }
             console.log('closing')
             if (pc) {
-                
+
                 // pc.setRemoteDescription(null)
                 pc.close()
                 pc = null
             }
-            
+
         }
-    }, [])
+    }, [roomName])
 
     useEffect(() => {
         onAudioStatusChange(isAudioEnabled);
@@ -147,13 +183,101 @@ const Video = forwardRef((props, ref) => {
                 setIsVideoEnabled(enabled);
             }
         },
-    }))
+    }));
+
+    useEffect(() => {
+        if (faceLandmarker && localVideoRef.current) {
+            const video = localVideoRef.current;
+    
+            video.addEventListener('loadedmetadata', () => {
+                // const canvasElement = document.createElement('canvas');
+                // canvasElement.width = video.videoWidth;
+                // canvasElement.height = video.videoHeight;
+                // canvasElement.style.position = 'absolute';
+                // canvasElement.style.top = '0';
+                // canvasElement.style.left = '0';
+                // canvasElement.style.pointerEvents = 'none';
+                // document.body.appendChild(canvasElement);
+                const canvas = canvasRef.current;
+                canvas.width = video.videoWidth;
+                canvas.height = video.videoHeight;
+    
+                const canvasCtx = canvas.getContext('2d');
+                const drawingUtils = new DrawingUtils(canvasCtx);
+    
+                const predictWebcam = async () => {
+                    if (!video.videoWidth || !video.videoHeight) {
+                        return;
+                    }
+    
+                    const results = await faceLandmarker.detectForVideo(video, performance.now());
+
+                    console.log('아ㅏ아아아ㅏ아아아아아아ㅏㅏㅏ', results);
+    
+                    if (results.faceLandmarks) {
+                        canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
+                        for (const landmarks of results.faceLandmarks) {
+                            drawingUtils.drawConnectors(
+                                landmarks,
+                                FaceLandmarker.FACE_LANDMARKS_TESSELATION,
+                                { color: "#C0C0C070", lineWidth: 1 }
+                            );
+                            drawingUtils.drawConnectors(
+                                landmarks,
+                                FaceLandmarker.FACE_LANDMARKS_RIGHT_EYE,
+                                { color: "#FF3030" }
+                            );
+                            drawingUtils.drawConnectors(
+                                landmarks,
+                                FaceLandmarker.FACE_LANDMARKS_RIGHT_EYEBROW,
+                                { color: "#FF3030" }
+                            );
+                            drawingUtils.drawConnectors(
+                                landmarks,
+                                FaceLandmarker.FACE_LANDMARKS_LEFT_EYE,
+                                { color: "#30FF30" }
+                            );
+                            drawingUtils.drawConnectors(
+                                landmarks,
+                                FaceLandmarker.FACE_LANDMARKS_LEFT_EYEBROW,
+                                { color: "#30FF30" }
+                            );
+                            drawingUtils.drawConnectors(
+                                landmarks,
+                                FaceLandmarker.FACE_LANDMARKS_FACE_OVAL,
+                                { color: "#E0E0E0" }
+                            );
+                            drawingUtils.drawConnectors(
+                                landmarks,
+                                FaceLandmarker.FACE_LANDMARKS_LIPS,
+                                { color: "#E0E0E0" }
+                            );
+                            drawingUtils.drawConnectors(
+                                landmarks,
+                                FaceLandmarker.FACE_LANDMARKS_RIGHT_IRIS,
+                                { color: "#FF3030" }
+                            );
+                            drawingUtils.drawConnectors(
+                                landmarks,
+                                FaceLandmarker.FACE_LANDMARKS_LEFT_IRIS,
+                                { color: "#30FF30" }
+                            );
+                        }
+                    }
+                    requestAnimationFrame(predictWebcam);
+                };
+    
+                predictWebcam();
+            });
+        }
+    }, [faceLandmarker]);
 
     return (
-        <>
+        <VideoContainer>
             <video ref={localVideoRef} playsInline id="left_cam" controls preload="metadata" autoPlay></video>
+            <Canvas ref={canvasRef} />
             <video ref={remoteVideoRef} playsInline id="right_cam" controls preload="metadata" autoPlay></video>
-        </>
+        </VideoContainer>
     );
 });
 export default Video;
