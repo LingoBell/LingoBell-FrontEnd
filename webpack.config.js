879a4bb8a6ab9d65f98ae694a1d1ca350b33f7c3
Ryan <tjddnfkdls0@naver.com>
2024-08-16 17:35:41 +0900

    live chat css

--- webpack.config.js
+++ webpack.config.js
@@ -69,7 +69,7 @@ module.exports = {
     }, {
       context: ['/socket.io'],
 
-      target: 'http://192.168.0.105:8080',
+      target: 'http://localhost:8080',
       // target: 'http://192.168.0.245:8080',
       ws:true
     }
